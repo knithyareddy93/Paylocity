6de52c5374656b2d79ee47911086c7aaee7ea1d7
knithyareddy93 <86015556+knithyareddy93@users.noreply.github.com>
2021-06-17 13:26:24 -0700

    Create App.js
    Default App

---
+++ App.js
@@ -0,0 +1,10 @@
+import FormComponent from "./components/FormComponent";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <FormComponent />
+    </div>
+  );
+}
